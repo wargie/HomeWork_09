8eccedf93f6debdb74b3d7083f333f6b092c9b36
wargie <wargie@bk.ru>
2022-03-03 13:29:32 +0200

    Поиск ошибок в коде к задаче №67

---
+++ Example_067/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 67: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//M = 1; N = 5. -> "1, 2, 3, 4, 5"
+//M = 4; N = 8. -> "4, 6, 7, 8"
+
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+if (M >= N)
+    {
+        Console.Write("Необходимо ввести N больше M");
+        return;
+    }
+    
+    for (int i = M; i <= N; i++) {
+        Console.Write("{0} ", i);
+    }
+
+
+/*void ShowNumberMToN(int M, N)
+{
+    if (M < N, M == N) 
+        {
+            Console.WriteLine("Число М должно быть больше N");
+        }
+    else 
+        {
+            if (number == 1) Console.Write(number);
+
+            else
+    {
+        Console.Write(number);
+        ShowNumberMToN(number - 1);
+    }
+}
+
+int number = 10;
+ShowNumberMToN(number);
+*/
