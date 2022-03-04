bcfd6977c4af435bb3844d3963357b5d619ae593
wargie <wargie@bk.ru>
2022-03-04 14:34:55 +0200

    Оптимизация кода к задаче задачи №71

--- Example_071/Program.cs
+++ Example_071/Program.cs
@@ -1,14 +1,20 @@
 ﻿//Задача 71: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
 //m = 2, n = 3 -> A(m,n) = 29
 
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
-int AckermanFunction(int m, int n)
-{
-    if (m == 0) return n + 1;
-    if (n == 0) return AckermanFunction(m - 1, 1);
-    return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
-}
+int AckermanFunction(int M, int N)
+    {
+        if (M != 0)
+            {
+                if (N != 0) return AckermanFunction(M - 1, AckermanFunction(M, N - 1));
+                return AckermanFunction(M - 1, 1);
+            }
 
-int m = 4;
-int n = 0;
-Console.WriteLine(AckermanFunction(m, n));
+        return N + 1;
+    }
+
+Console.WriteLine(AckermanFunction(M, N));
