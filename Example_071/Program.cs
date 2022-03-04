705c9a607c991c160c1f9774e627ff6ee33d31fd
wargie <wargie@bk.ru>
2022-03-04 14:30:02 +0200

    Начато решение задачи №71

---
+++ Example_071/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 71: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 29
+
+
+int AckermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if (n == 0) return AckermanFunction(m - 1, 1);
+    return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
+
+int m = 4;
+int n = 0;
+Console.WriteLine(AckermanFunction(m, n));
