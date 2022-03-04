f09214dcb0924bbb01b86dd356c934a8e45a1636
wargie <wargie@bk.ru>
2022-03-04 14:13:16 +0200

    Начато решение задачи №69

---
+++ Example_069/Program.cs
@@ -0,0 +1,8 @@
+﻿//Задача 69: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+//M = 1; N = 15 -> 15
+//M = 4; N = 8 -> 5
+
+Console.Write("Введите число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
